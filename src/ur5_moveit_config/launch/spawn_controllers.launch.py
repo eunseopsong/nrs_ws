40efd5ec4415f9920b33695f65c579a1ac11e9fa
eunseopsong <lexondms1@g.skku.edu>
2025-04-12 17:12:30 +0900

    separate ur5_config and ur10_config

--- src/ur5_moveit_config/launch/spawn_controllers.launch.py
+++ src/ur5_moveit_config/launch/spawn_controllers.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_spawn_controllers_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("ur5", package_name="ur10_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("ur5", package_name="ur5_moveit_config").to_moveit_configs()
     return generate_spawn_controllers_launch(moveit_config)
--- src/ur5_moveit_config/launch/static_virtual_joint_tfs.launch.py
+++ src/ur5_moveit_config/launch/static_virtual_joint_tfs.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_static_virtual_joint_tfs_laun
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("ur5", package_name="ur10_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("ur5", package_name="ur5_moveit_config").to_moveit_configs()
     return generate_static_virtual_joint_tfs_launch(moveit_config)
