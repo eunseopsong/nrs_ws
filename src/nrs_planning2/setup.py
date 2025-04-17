e3e6668996d02359d868e1e2a4fe92a7cff9985c
eunseopsong <lexondms1@g.skku.edu>
2025-04-17 17:44:32 +0900

    There is something wrong: fk_control node doesn't work

--- src/nrs_planning2/setup.py
+++ src/nrs_planning2/setup.py
@@ -5,6 +5,7 @@ package_name = 'nrs_planning2'
 setup(
     name=package_name,
     version='0.0.0',
+    # test 폴더만 제외하고, 최상위에 있는 src 패키지를 찾아 설치
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -15,12 +16,12 @@ setup(
     zip_safe=True,
     maintainer='eunseop',
     maintainer_email='lexondms1@g.skku.edu',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    description='Interpolation node for nrs_planning2',
+    license='Apache-2.0',
     entry_points={
         'console_scripts': [
-            'nrs_planning_node = nrs_planning2.planning_node:main',
+            # 모듈 이름을 'src'로 바꿔야, src 폴더가 설치된 패키지로 인식됨
+            'nrs_planning_node = src.planning_node:main',
         ],
     },
 )
