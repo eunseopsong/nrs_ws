46b3b6b54095ae9eafd31b498218d5d497697619
eunseopsong <lexondms1@g.skku.edu>
2025-04-17 15:58:29 +0900

    recreate: nrs_planning2

---
+++ src/nrs_planning2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'nrs_planning2'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='eunseop',
+    maintainer_email='lexondms1@g.skku.edu',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'nrs_planning_node = nrs_planning2.planning_node:main',
+        ],
+    },
+)
