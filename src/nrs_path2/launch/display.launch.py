64a6721132a47301c96dce6d74ebc1fe665f1104
eunseopsong <lexondms1@g.skku.edu>
2025-05-01 18:52:03 +0900

    ur_description pa package from /opt/ros/humble/share and trying move view_ur.launch.py

--- src/nrs_path2/launch/display.launch.py
+++ src/nrs_path2/launch/display.launch.py
@@ -1,46 +1,34 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 import os
 
-from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import Command
-from launch_ros.actions import Node
-from launch.substitutions import PathJoinSubstitution
-from launch.substitutions import LaunchConfiguration
-from launch.substitutions import TextSubstitution
-from launch_ros.substitutions import FindPackageShare
-from launch.substitutions import FindExecutable
-
 def generate_launch_description():
-
-    # 기본 설정
-    pkg_name = 'nrs_path2'
-    robot_file_name = 'ur10e.xacro'
-    pkg_path = get_package_share_directory(pkg_name)
-
-    urdf_path = os.path.join(pkg_path, 'urdf', robot_file_name)
-
-    # robot_description parameter 세팅
-    robot_description = {
-        'robot_description': Command([
-            PathJoinSubstitution([FindExecutable(name='xacro')]),
-            ' ',
-            urdf_path
-        ])
-    }
+    # URDF 파일 경로
+    urdf_path = os.path.join(
+        os.environ['HOME'],
+        'nrs_ws', 'src', 'urdf_files_dataset', 'urdf_files',
+        'ros-industrial', 'xacro_generated', 'universal_robots',
+        'ur_description', 'urdf', 'ur10e.urdf'
+    )
 
     return LaunchDescription([
+        # robot_state_publisher 실행
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
             name='robot_state_publisher',
             output='screen',
-            parameters=[robot_description]
+            parameters=[{'robot_description': open(urdf_path).read()}]
         ),
+
+        # rviz 실행
         Node(
             package='rviz2',
             executable='rviz2',
             name='rviz2',
-            output='screen'
-        )
+            output='screen',
+            arguments=[]
+        ),
     ])
