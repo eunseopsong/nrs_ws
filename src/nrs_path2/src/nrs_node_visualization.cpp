fc50cca790f60a34cde19fe29d8fcac1e4d474f0
eunseopsong <lexondms1@g.skku.edu>
2025-05-08 19:41:27 +0900

    nrs_node_visualization.cpp: Immigration Completega

--- src/nrs_path2/src/nrs_node_visualization.cpp
+++ src/nrs_path2/src/nrs_node_visualization.cpp
@@ -10,15 +10,27 @@
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "nrs_node_visualization");
-    ros::NodeHandle nh;
-    nrs_visualization visualizer;
-    visualizer.init(nh);
+    rclcpp::init(argc, argv);
+    auto node = rclcpp::Node::make_shared("nrs_node_visualization");
+    //// ros::init(argc, argv, "nrs_node_visualization");
+    //// ros::NodeHandle nh;
 
-    ros::Subscriber vis_waypoints_sub = nh.subscribe("interpolated_waypoints", 10, &nrs_visualization::waypointsCallback, &visualizer);
+    auto visualizer = std::make_shared<nrs_visualization>();
+    visualizer->init(node);
+    //// nrs_visualization visualizer;
+    //// visualizer.init(nh);
 
-    ROS_INFO("nrs_node_visualiztion started.visualization functionalities are active.");
+    auto subscription = node->create_subscription<nrs_path2::msg::Waypoints>(
+        "interpolated_waypoints", 10,
+        std::bind(&nrs_visualization::waypointsCallback, visualizer.get(), std::placeholders::_1));
+    //// ros::Subscriber vis_waypoints_sub = nh.subscribe("interpolated_waypoints", 10, &nrs_visualization::waypointsCallback, &visualizer);
 
-    ros::spin();
+    RCLCPP_INFO(node->get_logger(), "nrs_node_visualization started. Visualization functionalities are active.");
+    //// ROS_INFO("nrs_node_visualiztion started.visualization functionalities are active.");
+
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    ////ros::spin();
+
     return 0;
 }
