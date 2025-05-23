fa8a7262ba374bbd32c5d74149c1455079278af2
eunseopsong <lexondms1@g.skku.edu>
2025-05-23 17:32:25 +0900

    fimove: files except codes

---
+++ src/rtde_handarm2/NRS_yaml/NRS_yaml_location.h
@@ -0,0 +1,13 @@
+// Include guard was applied
+#pragma once
+#ifndef NRS_YAML_LOCATION_H
+#define NRS_YAML_LOCATION_H
+
+
+const char NRS_Fcon_desired_loc[] = "/home/nrsur10/catkin_ws/src/rtde_handarm/NRS_yaml/NRS_Fcon_desired.yaml";
+const char NRS_Fcon_setting_loc[] = "/home/nrsur10/catkin_ws/src/rtde_handarm/NRS_yaml/NRS_Fcon_setting.yaml";
+const char NRS_UR10_IP_loc[] = "/home/nrsur10/catkin_ws/src/rtde_handarm/NRS_yaml/NRS_UR10_IP.yaml";
+const char NRS_Record_Printing_loc[] = "/home/nrsur10/catkin_ws/src/rtde_handarm/NRS_yaml/NRS_Record_Printing.yaml";
+const char NRS_VR_setting_loc[] = "/home/nrsur10/catkin_ws/src/rtde_handarm/NRS_yaml/NRS_VR_setting.yaml";
+
+#endif
