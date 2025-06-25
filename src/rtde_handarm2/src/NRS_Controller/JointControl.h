987507133b8d491ef7b8c665b53814dde0f6c773
eunseopsong <lexondms1@g.skku.edu>
2025-06-25 15:26:29 +0900

    JointControl Class 다 비워놓고 시작

--- src/rtde_handarm2/src/NRS_Controller/JointControl.h
+++ src/rtde_handarm2/src/NRS_Controller/JointControl.h
@@ -3,10 +3,6 @@
 
 #include "func.h"
 
-// using namespace std;
-// using namespace Eigen;
-// using namespace std::chrono_literals;
-
 class JointControl : public rclcpp::Node
 {
 public:
@@ -16,25 +12,14 @@ private:
     void CalculateAndPublishJoint();
 
     // // Subscriber declarations
-    // rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr sub_bodypose;
-    // rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr sub_jointpos;
-    // rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr sub_jointvel;
-    // rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr sub_bodypos;
-    // rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr sub_bodyvel;
-    // rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr sub_imu;
-    // rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr sub_contact;
-    // rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr sub_command;
-    // rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr sub_distance;
-    // rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr sub_link_force;
+
 
     // // Publisher declarations
-    // rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr pub_torque;
-    // rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr pub_targetpos;
+
 
     // // Timer
-    // rclcpp::TimerBase::SharedPtr timer_;
-    // double _count;
-    // array<double, 3> previous_command; // save the previous command value
+
+
 };
 
 #endif // JOINTCONTROL_H
