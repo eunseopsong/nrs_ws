917c69e90dca4feaa6313f58faf584aa9c97ad86
eunseopsong <lexondms1@g.skku.edu>
2025-06-25 12:39:03 +0900

    main.cpp 분할 시도

--- src/rtde_handarm2/src/Yoon_UR10e_main.cpp
+++ src/rtde_handarm2/src/Yoon_UR10e_main.cpp
@@ -1297,3 +1297,14 @@ int main(int argc, char* argv[])
     return 0;
 
 }
+
+
+
+int main(int argc, char** argv)
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<JointControl>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
