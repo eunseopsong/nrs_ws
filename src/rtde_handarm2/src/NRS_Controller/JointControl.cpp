7ad6e40052fb0974cb26b6635143e6ccde44bdeb
eunseopsong <lexondms1@g.skku.edu>
2025-06-26 15:17:09 +0900

    this -> node_로 변경

--- src/rtde_handarm2/src/NRS_Controller/JointControl.cpp
+++ src/rtde_handarm2/src/NRS_Controller/JointControl.cpp
@@ -3,44 +3,44 @@
 #include <memory>
 
 JointControl::JointControl(const rclcpp::Node::SharedPtr& node)
-: rclcpp::Node("nrs_control_node"),  // 부모 생성자 호출 필요
-  node_(node)
+: node_(node)
+//   rclcpp::Node("nrs_control_node"),
 {
     AdaptiveK_msg_ = std::make_unique<nrs_msgmonitoring2::MsgMonitoring>(node_, "AdaptiveK_msg");
     FAAC3step_msg_ = std::make_unique<nrs_msgmonitoring2::MsgMonitoring>(node_, "FAAC3step_msg");
 
     // Publishers
-    YSurfN_Fext_pub_ = this->create_publisher<std_msgs::msg::Float64>("YSurfN_Fext", 20);
-    UR10e_mode_pub_  = this->create_publisher<std_msgs::msg::UInt16>("Yoon_UR10e_mode", 20);
-    UR10_Jangle_pub_ = this->create_publisher<std_msgs::msg::Float64MultiArray>("UR10_Jangle", 20);
-    UR10_pose_pub_   = this->create_publisher<std_msgs::msg::Float64MultiArray>("UR10_pose", 20);
-    UR10_wrench_pub_ = this->create_publisher<std_msgs::msg::Float64MultiArray>("UR10_wrench", 20);
+    YSurfN_Fext_pub_ = node_->create_publisher<std_msgs::msg::Float64>("YSurfN_Fext", 20);
+    UR10e_mode_pub_  = node_->create_publisher<std_msgs::msg::UInt16>("Yoon_UR10e_mode", 20);
+    UR10_Jangle_pub_ = node_->create_publisher<std_msgs::msg::Float64MultiArray>("UR10_Jangle", 20);
+    UR10_pose_pub_   = node_->create_publisher<std_msgs::msg::Float64MultiArray>("UR10_pose", 20);
+    UR10_wrench_pub_ = node_->create_publisher<std_msgs::msg::Float64MultiArray>("UR10_wrench", 20);
 
     // Subscribers
-    UR10e_mode_sub_ = this->create_subscription<std_msgs::msg::UInt16>(
+    UR10e_mode_sub_ = node_->create_subscription<std_msgs::msg::UInt16>(
         "/Yoon_UR10e_mode", 20,
-        std::bind(&JointControl::cmdModeCallback, this, std::placeholders::_1));
+        std::bind(&JointControl::cmdModeCallback, node_, std::placeholders::_1));
 
     PB_iter_sub_ = this->create_subscription<std_msgs::msg::UInt16>(
         "/Yoon_PbNum_cmd", 100,
-        std::bind(&JointControl::PbIterCallback, this, std::placeholders::_1));
+        std::bind(&JointControl::PbIterCallback, node_, std::placeholders::_1));
 
-    joint_cmd_sub_ = this->create_subscription<std_msgs::msg::Float64MultiArray>(
+    joint_cmd_sub_ = node_->create_subscription<std_msgs::msg::Float64MultiArray>(
         "/yoon_UR10e_joint_cmd", 100,
-        std::bind(&JointControl::JointCmdCallback, this, std::placeholders::_1));
+        std::bind(&JointControl::JointCmdCallback, node_, std::placeholders::_1));
 
-    VR_sub_ = this->create_subscription<geometry_msgs::msg::PoseStamped>(
+    VR_sub_ = node_->create_subscription<geometry_msgs::msg::PoseStamped>(
         "/vive/pos0", 100,
-        std::bind(&JointControl::VRdataCallback, this, std::placeholders::_1));
+        std::bind(&JointControl::VRdataCallback, node_, std::placeholders::_1));
 
-    joint_state_sub_ = this->create_subscription<sensor_msgs::msg::JointState>(
+    joint_state_sub_ = node_->create_subscription<sensor_msgs::msg::JointState>(
         "/isaac_joint_states", rclcpp::SensorDataQoS(),
-        std::bind(&JointControl::JointStateCallback, this, std::placeholders::_1));
+        std::bind(&JointControl::JointStateCallback, node_, std::placeholders::_1));
 
     // Timer
-    timer_ = this->create_wall_timer(
+    timer_ = node_->create_wall_timer(
         std::chrono::milliseconds(2),
-        std::bind(&JointControl::CalculateAndPublishJoint, this));
+        std::bind(&JointControl::CalculateAndPublishJoint, node_));
 }
 JointControl::~JointControl() {}
 
