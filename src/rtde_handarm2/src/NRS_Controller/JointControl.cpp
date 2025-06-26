39e4aed7c95099ca8f3c35959f3898b16c2863e3
eunseopsong <lexondms1@g.skku.edu>
2025-06-26 20:48:14 +0900

    *******중간백업*******

--- src/rtde_handarm2/src/NRS_Controller/JointControl.cpp
+++ src/rtde_handarm2/src/NRS_Controller/JointControl.cpp
@@ -676,16 +676,14 @@ void JointControl::CalculateAndPublishJoint()
 
     #endif
 
-    /////
+    ///// Add on 2025.06.26 /////
     // joint_pos 디버깅 출력
     printf("Current joint_pos values: ");
     for (int i = 0; i < 6; ++i) {
         printf("%.4f ", joint_pos[i]);
     }
     printf("\n");
-    /////
-
-
+    /////////////////////////////
 
     // spring mode control parameter
     VectorXd Hspring_mode_init_pos(6);
