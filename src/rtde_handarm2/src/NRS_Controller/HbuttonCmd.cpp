c2e153d9d706e82c10c182e4e5d14fec8ab91f81
eunseopsong <lexondms1@g.skku.edu>
2025-06-27 23:19:58 +0900

    Hbutton 생성자

--- src/rtde_handarm2/src/NRS_Controller/HbuttonCmd.cpp
+++ src/rtde_handarm2/src/NRS_Controller/HbuttonCmd.cpp
@@ -5,36 +5,74 @@
 HbuttonCmd::HbuttonCmd()
 : Node("hbutton_node"), loop_rate(100.0), fin1(NRS_Record_Printing_loc), fin2(NRS_Fcon_desired_loc)
 {
-    ///////////////// 1. ROS Publisher 초기화 //////////////////
-    yoon_mode_pub = this->create_publisher<std_msgs::msg::UInt16>("Yoon_UR10e_mode", 10);
-    PbNum_command_pub = this->create_publisher<std_msgs::msg::UInt32>("Yoon_PbNum_cmd", 10);
-    Clicked_pub = this->create_publisher<geometry_msgs::msg::PointStamped>("clicked_point", 10);
+    ////// 1. UART init ///////
+#if (Handle_OnOff == 1)
+    Yuart = new Yoon_UART("/dev/ttyACM0", 115200);
+#endif
 
-    ///////////////// 2. ROS Subscriber 초기화 //////////////////
+    ////// 2. YAML file load ///////
+    NRS_recording = YAML::Load(fin1);
+    NRS_Fcon_desired = YAML::Load(fin2);
+
+    ////// 3. ROS Publishers ///////
+    yoon_mode_pub = this->create_publisher<std_msgs::msg::UInt16>("Yoon_UR10e_mode", 20);
+    PbNum_command_pub = this->create_publisher<std_msgs::msg::UInt32>("Yoon_PbNum_cmd", 20);
+    Clicked_pub = this->create_publisher<geometry_msgs::msg::PointStamped>("clicked_point", 20);
+
+    ////// 4. ROS Subscriber ///////
     VRPose_sub = this->create_subscription<geometry_msgs::msg::PoseStamped>(
-        "/vive/pos0", 10,
+        "/pos_cal_rviz", 20,
         std::bind(&HbuttonCmd::VRPose_Callback, this, std::placeholders::_1));
 
-    ///////////////// 3. ROS Service 서버 초기화 //////////////////
+    ////// 5. ROS Service Servers ///////
     Aidin_gui_srv1 = this->create_service<std_srvs::srv::Empty>(
-        "Aidin_gui_srv1",
+        "teaching_mode",
         std::bind(&HbuttonCmd::SRV1_Handle, this, std::placeholders::_1, std::placeholders::_2));
 
     Aidin_gui_srv3 = this->create_service<std_srvs::srv::Empty>(
-        "Aidin_gui_srv3",
+        "save_waypoint",
         std::bind(&HbuttonCmd::SRV3_Handle, this, std::placeholders::_1, std::placeholders::_2));
 
     Aidin_gui_srv4 = this->create_service<std_srvs::srv::Empty>(
-        "Aidin_gui_srv4",
+        "trajectory_generation",
         std::bind(&HbuttonCmd::SRV4_Handle, this, std::placeholders::_1, std::placeholders::_2));
 
     Aidin_gui_srv11 = this->create_service<std_srvs::srv::Empty>(
-        "Aidin_gui_srv11",
+        "Iteration_set",
         std::bind(&HbuttonCmd::SRV11_Handle, this, std::placeholders::_1, std::placeholders::_2));
 
     Aidin_gui_srv12 = this->create_service<std_srvs::srv::Empty>(
-        "Aidin_gui_srv12",
+        "Playback_execution",
         std::bind(&HbuttonCmd::SRV12_Handle, this, std::placeholders::_1, std::placeholders::_2));
+
+    ////// 6. State & Mode Initialization ///////
+#if (TEACHING_MODE == 0)
+    #if (Handle_OnOff == 0)
+        current_status = "Stanby mode - No Handle";
+        mode0 = "Stanby mode - No Handle";
+        mode1 = "Teaching mode - No Handle";
+    #elif (Handle_OnOff == 1)
+        current_status = "Handle stanby mode";
+        mode0 = "Handle stanby mode";
+        mode1 = "Handle-guiding mode";
+    #endif
+#elif (TEACHING_MODE == 1)
+    current_status = "VR stanby mode";
+    mode0 = "VR stanby mode";
+    mode1 = "VR-teaching mode";
+#endif
+
+    mode2 = "Playback ready";
+    mode3 = "Playback execution";
+    mode4 = "Path generation done";
+    modeErr1 = "Wrong iter number(1~9)";
+
+    Hmode0 = "MODE 0";  // Default value
+    Hmode1 = "MODE 1";  // Mode change
+    Hmode2 = "MODE 2";  // Way point save
+    Hmode3 = "MODE 3";  // Playback start
+    Hmode4 = "MODE 4";  // Iteration number
+    Hmode5 = "MODE 5";  // Trajectory generation
 }
 
 
