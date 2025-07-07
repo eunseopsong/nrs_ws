d633bac878a794b8f2e69aba6bc72f43d6059b80
eunseopsong <lexondms1@g.skku.edu>
2025-07-07 16:01:43 +0900

    catch signal path change

--- src/rtde_handarm2/src/NRS_Controller/node_Hbutton.cpp
+++ src/rtde_handarm2/src/NRS_Controller/node_Hbutton.cpp
@@ -1,4 +1,11 @@
 #include "HbuttonCmd.h"
+#include <signal.h>
+
+void catch_signal(int sig) {
+    printf("Program was terminated \n");
+    rclcpp::shutdown();
+    exit(1);
+}
 
 int main(int argc, char **argv)
 {
@@ -6,11 +13,28 @@ int main(int argc, char **argv)
 
     auto node = std::make_shared<HbuttonCmd>();
 
-    rclcpp::spin(node);
+    // 시그널 핸들러 등록
+    signal(SIGTERM, catch_signal);
+    signal(SIGINT, catch_signal);
+
+    // 사용자 정의 main loop 실행 (while 루프)
+    node->HButton_main();
 
     rclcpp::shutdown();
     return 0;
 }
+
+// int main(int argc, char **argv)
+// {
+//     rclcpp::init(argc, argv);
+
+//     auto node = std::make_shared<HbuttonCmd>();
+
+//     rclcpp::spin(node);
+
+//     rclcpp::shutdown();
+//     return 0;
+// }
 
 // int main(int argc, char **argv)
 // {
