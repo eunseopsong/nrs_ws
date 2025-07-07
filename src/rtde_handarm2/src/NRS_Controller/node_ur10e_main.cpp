3e835a7171c790c92d08bb23860214238a78be16
eunseopsong <lexondms1@g.skku.edu>
2025-07-07 14:43:51 +0900

    node_ur10e_main: mutithread

--- src/rtde_handarm2/src/NRS_Controller/node_ur10e_main.cpp
+++ src/rtde_handarm2/src/NRS_Controller/node_ur10e_main.cpp
@@ -1,15 +1,25 @@
 #include "JointControl.h"
+#include <rclcpp/executors/multi_threaded_executor.hpp>
 
 int main(int argc, char* argv[])
 {
     rclcpp::init(argc, argv);
+
+    // 노드 생성
     auto node = std::make_shared<rclcpp::Node>("nrs_control_node");
 
-    // 생성자에 node 넘기기
+    // JointControl 클래스 객체 생성
     auto joint_ctrl = std::make_shared<JointControl>(node);
 
-    rclcpp::spin(node);
+    // 멀티스레드 executor 생성
+    rclcpp::executors::MultiThreadedExecutor executor;
+
+    // 노드 추가
+    executor.add_node(node);
+
+    // 콜백을 병렬로 처리
+    executor.spin();
+
     rclcpp::shutdown();
     return 0;
 }
-
