2f9243fd0dcbd7d08ec049e680ec96b08c4373c9
eunseopsong <lexondms1@g.skku.edu>
2025-07-18 14:37:07 +0900

    tic_emkm: data load

--- src/nrs_dlc/nrs_dlc/tic_emkm.py
+++ src/nrs_dlc/nrs_dlc/tic_emkm.py
@@ -4,13 +4,13 @@ import json
 import matplotlib.pyplot as plt
 from scipy.ndimage import gaussian_filter1d
 
-def load_and_preprocess_tic_data(data_num=1, base_folder="data/TIC/training"):
+def load_and_preprocess_tic_data(data_num=2, base_folder="data/TIC/training"):
     """
     TIC 데이터를 불러오고 전처리를 수행합니다.
     Returns:
         raw_vel:       선형/각속도 벡터 (Nx6)
         TWC_comp_FT:   보정된 Force/Torque (Nx6)
-        raw_FT:        원본 측정된 Force/Torque (Nx6)
+        ori_FT:        원본 측정된 Force/Torque (Nx6)
     """
     # 현재 스크립트 위치 기준으로 절대경로 계산
     current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -25,22 +25,22 @@ def load_and_preprocess_tic_data(data_num=1, base_folder="data/TIC/training"):
         # 앞 28줄 제거 (MATLAB의 raw_xxx(1:28,:) = []와 동일)
         raw_vel = np.loadtxt(file_vel, dtype=float, ndmin=2)[28:, :]
         TWC_comp_FT = np.loadtxt(file_TWC_comp, dtype=float, ndmin=2)[28:, :]
-        raw_FT = np.loadtxt(file_ori_ft, dtype=float, ndmin=2)[28:, :]
+        ori_FT = np.loadtxt(file_ori_ft, dtype=float, ndmin=2)[28:, :]
     except Exception as e:
         raise RuntimeError(f"❌ 파일 읽기 실패: {e}")
 
     print("✅ TIC 데이터 로드 및 전처리 완료")
     print(f" - raw_vel shape: {raw_vel.shape}")
     print(f" - TWC_comp_FT shape: {TWC_comp_FT.shape}")
-    print(f" - raw_FT shape: {raw_FT.shape}")
+    print(f" - raw_FT shape: {ori_FT.shape}")
 
-    return raw_vel, TWC_comp_FT, raw_FT
+    return raw_vel, TWC_comp_FT, ori_FT
 
 def main():
     print("✅ TIC KalmanEM Node 시작")
 
     try:
-        raw_vel, TWC_comp_FT, raw_FT = load_and_preprocess_tic_data(data_num=1)
+        raw_vel, TWC_comp_FT, ori_FT = load_and_preprocess_tic_data(data_num=2)
     except RuntimeError as e:
         print(e)
         return
