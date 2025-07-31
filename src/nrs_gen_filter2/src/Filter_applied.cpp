a6ba9cea11368ee0a08ef7aad55854e4d767804d
eunseopsong2 <lexondms1@g.skku.edu>
2025-08-01 06:18:22 +0900

    nrs_gen_filter2: just paste file

---
+++ src/nrs_gen_filter2/src/Filter_applied.cpp
@@ -0,0 +1,19 @@
+#include "Filter_applied.hpp"
+
+NRS_ACC_calculation::NRS_ACC_calculation() 
+{
+    x_pre = 0;
+    p_pre = 0;
+}
+
+double NRS_ACC_calculation::vel2Acc(double velocity_input)
+{
+    double cal_acc;
+
+    cal_acc = (velocity_input - vel_pre) / dt;
+
+    cal_acc = KalmanFilter1D(cal_acc);
+    vel_pre = velocity_input;
+
+    return cal_acc;
+}
