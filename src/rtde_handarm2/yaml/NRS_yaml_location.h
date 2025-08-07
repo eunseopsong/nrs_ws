8c418f84de3e966926835766ca61bff6d3e931d2
eunseopsong2 <lexondms1@g.skku.edu>
2025-08-07 14:51:33 +0900

    yaml 경로 수정

--- src/rtde_handarm2/yaml/NRS_yaml_location.h
+++ src/rtde_handarm2/yaml/NRS_yaml_location.h
@@ -3,10 +3,10 @@
 #ifndef NRS_YAML_LOCATION_H
 #define NRS_YAML_LOCATION_H
 
-const char NRS_Fcon_desired_loc[] = "/home/eunseop/nrs_ws/src/rtde_handarm2/NRS_yaml/NRS_Fcon_desired.yaml";
-const char NRS_Fcon_setting_loc[] = "/home/eunseop/nrs_ws/src/rtde_handarm2/NRS_yaml/NRS_Fcon_setting.yaml";
-const char NRS_UR10_IP_loc[] = "/home/eunseop/nrs_ws/src/rtde_handarm2/NRS_yaml/NRS_UR10_IP.yaml";
-const char NRS_Record_Printing_loc[] = "/home/eunseop/nrs_ws/src/rtde_handarm2/NRS_yaml/NRS_Record_Printing.yaml";
-const char NRS_VR_setting_loc[] = "/home/eunseop/nrs_ws/src/rtde_handarm2/NRS_yaml/NRS_VR_setting.yaml";
+const char NRS_Fcon_desired_loc[] = "/home/eunseop/nrs_ws/src/rtde_handarm2/yaml/NRS_Fcon_desired.yaml";
+const char NRS_Fcon_setting_loc[] = "/home/eunseop/nrs_ws/src/rtde_handarm2/yaml/NRS_Fcon_setting.yaml";
+const char NRS_UR10_IP_loc[] = "/home/eunseop/nrs_ws/src/rtde_handarm2/yaml/NRS_UR10_IP.yaml";
+const char NRS_Record_Printing_loc[] = "/home/eunseop/nrs_ws/src/rtde_handarm2/yaml/NRS_Record_Printing.yaml";
+const char NRS_VR_setting_loc[] = "/home/eunseop/nrs_ws/src/rtde_handarm2/yaml/NRS_VR_setting.yaml";
 
 #endif
