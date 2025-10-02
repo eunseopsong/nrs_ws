a6e09177a9a0b56a4d35e3a3c69e8025331574d3
eunseopsong2 <lexondms1@g.skku.edu>
2025-10-02 13:29:13 +0900

    push from LAb

--- src/rtde_handarm2/src/JointControl.cpp
+++ src/rtde_handarm2/src/JointControl.cpp
@@ -709,8 +709,8 @@ if (control_mode == 3) {
 
   // 🔧 스핀들 보정값 (TCP가 실제로 Z축으로 이만큼 내려가 있음)
 
-  constexpr double TOOL_Z = -0.35; // ntact_force = 0N (for concave test)
-  // constexpr double TOOL_Z = -0.269; // [m] contact_force = 0N (success in convex & concave surface)
+  // constexpr double TOOL_Z = -0.35; // ntact_force = 0N (for concave test)
+  constexpr double TOOL_Z = -0.269; // [m] contact_force = 0N (success in convex & concave surface)
   // constexpr double TOOL_Z = -0.265; // [m] contact_force = 0N
   // constexpr double TOOL_Z = -0.2615; // ntact_force = 0N
   // constexpr double TOOL_Z = -0.25; // [m] contact_force = 0N
