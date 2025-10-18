c8418ad60202c9916faa3e86c1d81c32fffa61c8
eunseopsong <lexondms1@g.skku.edu>
2025-10-19 05:06:59 +0900

    nrs_path2 경로 변경

--- src/nrs_path2/src/path_projection.cpp
+++ src/nrs_path2/src/path_projection.cpp
@@ -387,7 +387,7 @@ public:
     // mesh_file_path_     = this->declare_parameter<std::string>("mesh_file_path", "/home/eunseop/nrs_ws/src/nrs_path2/mesh/non_continuous_surface_2.stl");
     plane_path_file_path_ = this->declare_parameter<std::string>("plane_path_file_path",
                               // "/home/eunseop/nrs_ws/src/nrs_path2/data/Ori_path_transformed.txt");
-                              "/home/eunseop/nrs_ws/src/nrs_path2/data/geodesic_waypoints.txt");
+                              "/home/eunseop/nrs_ws/src/nrs_path2/data/final_waypoints.txt");
 
 
                               
