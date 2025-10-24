a5bafc30bb15c2a74c8b08bd92077031c0bdba17
eunseopsong <lexondms1@g.skku.edu>
2025-10-25 00:16:46 +0900

    *I Applied FAAC, but the contact force did not be stabilized*

--- src/rtde_handarm2/include/rtde_handarm2/ForceControl/admittance_control.hpp
+++ src/rtde_handarm2/include/rtde_handarm2/ForceControl/admittance_control.hpp
@@ -1,25 +1,25 @@
 #pragma once
-#include <array>
+#include <stdio.h>
 #include <cmath>
 
-class Yadmittance_control {
-public:
-    explicit Yadmittance_control(double sampling_time);
-
-    bool   adm_1D_MDK(double Mass, double Damping, double Stiffness);
-    double adm_MDK_monitor(int select);   // 0:M, 1:D, 2:K
-    double adm_1D_control(double xd, double Fd, double Fext);
-
+class Yadmittance_control
+{
 private:
-    // params
-    double adm_1D_dt{0.002};
-    double adm_1D_M{1.0}, adm_1D_D{10.0}, adm_1D_K{0.0};
+    double adm_1D_Perror[3] = {0,};
+    double adm_1D_Ferror[3] = {0,};
+    double adm_1D_M = 1;
+    double adm_1D_D = 1;
+    double adm_1D_K = 0;
+    double adm_1D_A, adm_1D_B, adm_1D_C;
+    double adm_1D_dt;
+    double xc;
 
-    // discrete internal states
-    double xc{0.0};     // last commanded position (internal)
-    double x{0.0};      // last output position
-    double v{0.0};      // last output velocity
+public:
+    Yadmittance_control(){};
+    Yadmittance_control(double sampling_time);
+    ~Yadmittance_control() {}
 
-    std::array<double,3> adm_1D_Perror{0.0,0.0,0.0};
-    std::array<double,3> adm_1D_Ferror{0.0,0.0,0.0};
+    bool adm_1D_MDK(double Mass, double Damping, double Stiffness);
+    double adm_MDK_monitor(int select);
+    double adm_1D_control(double xd, double Fd, double Fext);
 };
